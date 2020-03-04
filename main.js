bfdc33e81509ccae62b968c05f647a439a5199c5
Ben Cole <benedictcole1@gmail.com>
2020-03-04 12:28:00 +1030

    can't get basic syntax

--- main.js
+++ main.js
@@ -1,1 +1,4 @@
-let alphabetLower = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+const alphabetLower = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+let alphabetLowerChoice = alphabetLower.charAt(4);
+
+console.log(alphabetLowerChoice);
