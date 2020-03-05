9184467f36f5a82366fac7591dc5f850b0606940
Ben Cole <benedictcole1@gmail.com>
2020-03-05 12:46:37 +1030

    created responsive page elements

--- main.js
+++ main.js
@@ -1,4 +1,16 @@
-const alphabetLower = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-let alphabetLowerChoice = alphabetLower.charAt(4);
+// Password Generator functionality
 
-console.log(alphabetLowerChoice);
+var alphabetLower = "abcdefghijklmnopqrstuvwxyz";
+var alphabetUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numbers = "0123456789";
+var specialCharacters = " !\"#$%&'()*+,-./:;<=>?@[]^_`{|}~"
+
+function randomNum() {
+  return Math.floor(Math.random() * 26);
+}
+// strings have methods, like .charAt()
+function alphabetLowerResult() {
+  return alphabetLower.charAt(randomNum());
+}
+// activate function code with ()
+console.log(alphabetLowerResult());
